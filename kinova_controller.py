9d6e3f3c55291c9a6ce691b9169a9aca70379ecc
Chen_Tzu-Tung_tchen604 <tchen604@MEL06556D.me.gatech.edu>
2024-11-15 18:41:42 -0500

    Upload: kinova_controller.py

---
+++ kinova_controller.py
@@ -0,0 +1,11 @@
+# 1. Generated camera positions
+#   => How to make the robotis arm understand the positions of the generated camera viewpoints (on a sphere) relative to the robot's frame of reference?
+
+# 2. Use the intrinsic parameters of the microscope in NeRF Training
+#   => Calibrate the camera (Learn how to calibrate microscope)
+#   => Save the intrinsics (the intrinsics in calibration file is for the endoscope)
+#   => Use intrinsics in NeRF Training
+
+# 3. Move the arm to each camera position
+# 4. Capture and save images
+# 5. Train NeRF
